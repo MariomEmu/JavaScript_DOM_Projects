710badb638b7cce8b3627a790b7b44b6fa32ee8b
Mariom Emu <69960457+MariomEmu@users.noreply.github.com>
2025-04-12 21:28:01 +0600

    Add files via upload

---
+++ Image to text converter/script.js
@@ -0,0 +1,24 @@
+const extractText = () => {
+  const fileInput = document.getElementById('imageInput');
+  const outPutDiv = document.getElementById('output');
+
+  const imagesFile = fileInput.files[0];
+
+
+  if(!imagesFile) {
+    outPutDiv.textContent = 'please select an image file'
+    return;
+  }
+
+  Tesseract.recognize(
+    imagesFile,
+    'eng'
+  ).then(({data}) => {
+    outPutDiv.textContent = data.text;
+  }).catch((error) => {
+    console.error('Error', error);
+    outPutDiv.textContent = 'Error';
+  })
+
+}
+
